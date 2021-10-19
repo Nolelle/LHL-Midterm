02fba59cf48d01bb2ba95a858b679b2448f95ff8
wagabooga <mattanthu@hotmail.com>
2021-10-19 03:57:53 +0000

    Made sure all templates have cookie check for nav bar, as well as refactored templates

--- routes/templates/index.js
+++ routes/templates/index.js
@@ -1,8 +1,5 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All template routes for localhost:8080:/ are defined here
  */
 
 const express = require("express");
--- routes/templates/logout.js
+++ routes/templates/logout.js
@@ -1,15 +1,11 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All template routes for localhost:8080:/logout are defined here
  */
 
 const express = require("express");
 const router = express.Router();
 
 module.exports = () => {
-  //POST logout
   router.post("/", (req, res) => {
     res.clearCookie("email");
     res.redirect("/");
