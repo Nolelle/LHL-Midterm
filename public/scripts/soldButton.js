e08ec268962b5347f9be7294f5d76679f00bce07
Edmund Yu <eyu1@ualberta.ca>
2021-10-21 03:09:00 +0000

    Add jquery to singleListing.ejs and js script. Retrieved listing data from /api/resources/:id

--- public/scripts/soldButton.js
+++ public/scripts/soldButton.js
@@ -1,13 +1,49 @@
-//document ready function
 //reads the boolean field to see if item is sold or not,
 //when i submit mark as sold button,
 //this updates the database
 //for that userID, and listingID the sold field
+
+//document ready function
 $(() => {
-  $(".sold-button").on("submit", function (event) {
-    console.log(event);
-    //   const listingId = req.params.id;
-    //   $.get("/api/listings/${userID}").then(() => {});
-    // });
-  });
+  const url = window.location.pathname;
+  const id = url.substring(url.lastIndexOf("/") + 1);
+  const createNotSoldButton = () => {
+    const $notsoldButton = $(
+      `
+      <div class ='sold-button'>
+        <button type="submit" class="btn btn-primary">
+          Mark as sold
+        </button>
+      </div>
+      `
+    );
+    return $notsoldButton;
+  };
+  console.log(createNotSoldButton());
+  const createSoldButton = () => {
+    const $soldButton = $(
+      `
+      <div class ='sold-button'>
+        <button type="submit" class="btn btn-primary">
+          Mark as sold
+        </button>
+      </div>
+      `
+    );
+    return $soldButton;
+  };
+  const renderButton = () => {
+    $.get(`/api/listings/${id}`)
+      .then((listing) => {
+        const listingStatus = listing.sold;
+        if (listingStatus) {
+          createSoldButton();
+        } else {
+          createNotSoldButton();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 });
